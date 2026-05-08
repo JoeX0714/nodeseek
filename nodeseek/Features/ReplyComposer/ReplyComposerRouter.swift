788b512546b5f3461ffd86b5f0750dedf1089eb6
XIA RUOXIANG <xia.r.xgoole@gmail.com>
2026-05-08 13:02:24 +0800

    Refactor createModule to use MainActor for isolation

--- nodeseek/Features/ReplyComposer/ReplyComposerRouter.swift
+++ nodeseek/Features/ReplyComposer/ReplyComposerRouter.swift
@@ -9,26 +9,27 @@ import Foundation
 import UIKit
 
 class ReplyComposerRouter: ReplyComposerRouterProtocol {
-    
+
     // MARK: - Properties
     weak var viewController: UIViewController?
-    
+
     // MARK: - Static Methods
     static func createModule() -> UIViewController {
-        let router = ReplyComposerRouter()
-        let interactor = ReplyComposerInteractor()
-        let presenter = ReplyComposerPresenter(
-            interactor: interactor,
-            router: router
-        )
-        
-        interactor.presenter = presenter
-        
-        let view = ReplyComposerViewController(presenter: presenter)
-        
-        presenter.setView(view)
-        router.viewController = view
-        
-        return view
+        MainActor.assumeIsolated {
+            let router = ReplyComposerRouter()
+            let interactor = ReplyComposerInteractor()
+            let presenter = ReplyComposerPresenter(
+                interactor: interactor,
+                router: router
+            )
+
+            interactor.presenter = presenter
+
+            let view = ReplyComposerViewController(presenter: presenter)
+            presenter.setView(view)
+            router.viewController = view
+
+            return view
+        }
     }
 }
